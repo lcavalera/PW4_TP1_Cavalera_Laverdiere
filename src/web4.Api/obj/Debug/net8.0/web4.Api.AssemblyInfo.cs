408ee11f17ecdc862809a22493c202a3746a4057
Frederic Laverdiere <0838286@cegeplimoilou.ca>
2024-01-23 10:34:57 -0500

    c

--- src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
+++ src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b658e89b8d4cd21660faff346b24d16c07ba0189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a4be6257639911f7c55c8fa050ca4c4ef43474")]
 [assembly: System.Reflection.AssemblyProductAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
