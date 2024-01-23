1b75cb297d175a8f495bbee9941f3a28b4d6ec6a
Frederic Laverdiere <0838286@cegeplimoilou.ca>
2024-01-23 10:46:57 -0500

    ajout: data/Repository

---
+++ src/web4.Api/Data/Repository.cs
@@ -0,0 +1,41 @@
+﻿using web4.Api.Entites;
+
+namespace web4.Api.Data
+{
+    public static class Repository
+    {
+        private static int _ID_SEQUENCE_PRODUCT { get; set; } = 1;
+        public static ISet<Personne> Personnes { get; set; } = new HashSet<Personne>();
+        public static ISet<Evenement> Evenements { get; set; } = new HashSet<Evenement>();
+        public static ISet<Ville> Villes { get; set; } = new HashSet<Ville>();
+
+
+        public static Personne AddPersonne(Personne personne)
+        {
+
+            personne.Id = _ID_SEQUENCE_PRODUCT++;
+            Personnes.Add(personne);
+            Personnes.Add(personne);
+
+            return personne;
+        }
+        public static Evenement AddEvenement(Evenement evenement)
+        {
+
+            evenement.Id = _ID_SEQUENCE_PRODUCT++;
+            Evenements.Add(evenement);
+            Evenements.Add(evenement);
+
+            return evenement;
+        }
+        public static Ville AddVille(Ville ville)
+        {
+
+            ville.Id = _ID_SEQUENCE_PRODUCT++;
+            Villes.Add(ville);
+            Villes.Add(ville);
+
+            return ville;
+        }
+    }
+}
--- src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
+++ src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a4be6257639911f7c55c8fa050ca4c4ef43474")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408ee11f17ecdc862809a22493c202a3746a4057")]
 [assembly: System.Reflection.AssemblyProductAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
