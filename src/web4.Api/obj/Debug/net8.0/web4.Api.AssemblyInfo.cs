c49ba44720086de65c89307c3cdc7f62cf0a1434
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-01-22 21:16:08 -0500

    valid merge

--- src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
+++ src/web4.Api/obj/Debug/net8.0/web4.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbae2cac26d047ecd459a50aca0968298d0c67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web4.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
