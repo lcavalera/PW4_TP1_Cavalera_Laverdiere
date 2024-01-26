a840fa1d1202f18746b2eb2837acf27dd24ba6bf
Frederic Laverdiere <0838286@cegeplimoilou.ca>
2024-01-26 09:46:02 -0500

    ajout: doc swagger categorie

--- src/web4.Api/Controllers/CategorieController.cs
+++ src/web4.Api/Controllers/CategorieController.cs
@@ -13,6 +13,18 @@ namespace Events.Api.Controllers
     public class CategorieController(ICategorieBL categorieBL) : ControllerBase
     {
         private readonly ICategorieBL _categorieBL = categorieBL;
+
+        /// <summary>
+        /// Retourne une liste des Categories 
+        /// </summary>
+        /// <remarks>
+        /// 
+        ///     GET api/Categorie
+        ///
+        /// </remarks>
+        /// <response code="200">Categories trouvés et retournés</response>
+        /// <response code="500">service indisponible pour le moment</response>
+        /// <returns></returns>
         // GET: api/<CategorieController>
         [HttpGet]
         [ProducesResponseType(typeof(List<Categorie>), StatusCodes.Status200OK)]
@@ -22,6 +34,19 @@ namespace Events.Api.Controllers
             return Ok(_categorieBL.ObtenirTout());
         }
 
+
+        /// <summary>
+        /// Retourne une Categorie spécifique à partir de son id
+        /// </summary>
+        /// <param name="id">id de la Categorie à retourner</param>
+        /// <remarks>
+        /// 
+        ///     GET /Categorie/1
+        ///
+        /// </remarks>
+        /// <response code="200">Categorie trouvé et retourné</response>
+        /// <response code="404">Categorie introuvable pour l'id spécifié</response>
+        /// <response code="500">service indisponible pour le moment</response>
         // GET api/<CategorieController>/5
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status200OK)]
@@ -33,6 +58,26 @@ namespace Events.Api.Controllers
             return categorie != null ? Ok(categorie) : NotFound();
         }
 
+        /// <summary>
+        /// Ajoute une Categorie à la base de donnée
+        /// </summary>
+        /// <param name="categorie">Categorie à ajouter</param>
+        /// <returns>Une nouvelle Categorie a été créé</returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Categorie
+        ///     {
+        ///        "id": 1,
+        ///        "nom": "nom de la Categorie"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Categorie ajouté avec succès</response>
+        /// <response code="200">traitement executé avec succès, contenu retourné</response>
+        /// <response code="204">traitement executé avec succès, aucune contenu retourné</response>
+        /// <response code="400">model Invalide, mauvaise requête</response>
+        /// <response code="500">service indisponible pour le moment</response>
         // POST api/<CategorieController>
         [HttpPost]
         [Consumes("application/json")]
@@ -40,7 +85,7 @@ namespace Events.Api.Controllers
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status204NoContent)]
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(Ville), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Categorie), StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public IActionResult Post([FromBody] Categorie categorie)
         {
@@ -48,6 +93,17 @@ namespace Events.Api.Controllers
             return CreatedAtAction(nameof(GetById), new { id = categorie.Id }, null);
         }
 
+        /// <summary>
+        /// Modification d'une Categorie
+        /// </summary>
+        /// <param name="id">id de la Categorie à modifier</param>
+        /// <param name="categorie"></param>
+        /// <returns>La Categorie a été modifié</returns>
+        /// <response code="200">traitement executé avec succès, contenu retourné</response>
+        /// <response code="204">Categorie modifié avec succès, aucune contenu retourné</response>
+        /// <response code="400">model Invalide, mauvaise requête</response>
+        /// <response code="404">Categorie introuvable pour l'id spécifié</response>
+        /// <response code="500">service indisponible pour le moment</response>
         // PUT api/<CategorieController>/5
         [HttpPut("{id}")]
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status200OK)]
@@ -61,6 +117,13 @@ namespace Events.Api.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Supprime une Categorie
+        /// </summary>
+        /// <param name="id">id de la Categorie à supprimer</param>
+        /// <response code="204">Categorie supprimé avec succès, aucune contenu retourné</response>
+        /// <response code="404">Categorie introuvable pour l'id spécifié</response>
+        /// <response code="500">service indisponible pour le moment</response>
         // DELETE api/<CategorieController>/5
         [HttpDelete("{id}")]
         [ProducesResponseType(typeof(Categorie), StatusCodes.Status204NoContent)]
