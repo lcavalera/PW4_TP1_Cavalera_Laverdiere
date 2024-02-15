da511510e922a974cb671cb133faee09e8edd895
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-02-15 01:48:01 -0500

    corr evenementsBL

--- src/web4.Api/Entites/MappingProfiles.cs
+++ src/web4.Api/Entites/MappingProfiles.cs
@@ -5,11 +5,19 @@ namespace Events.Api.Entites
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() {
+        public MappingProfile()
+        {
             CreateMap<Categorie, CategorieDTO>();
             CreateMap<Participation, ParticipationDTO>();
             CreateMap<Ville, VilleDTO>();
-            CreateMap<Evenement, EvenementDTO>();
+
+            CreateMap<Evenement, EvenementDTO>()
+                .ForMember(dest => dest.CategoriesIds, opt => opt.MapFrom(src => src.Categories.Select(c => c.Id).ToList()));
+
+
+
+            //CreateMap<EvenementDTO, Evenement>()
+            //    .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.CategoriesIds));
         }
     }
 }
