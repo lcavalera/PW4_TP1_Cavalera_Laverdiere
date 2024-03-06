fb8a3e1479650d52281e0632bd80f5fb55cd8b45
Frederic Laverdiere <0838286@cegeplimoilou.ca>
2024-03-06 14:26:24 -0500

    d

--- src/IdentityServerAspNetIdentity/obj/Debug/net7.0/IdentityServerAspNetIdentity.AssemblyInfo.cs
+++ src/IdentityServerAspNetIdentity/obj/Debug/net7.0/IdentityServerAspNetIdentity.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a76e62fe3bf5f09d74a3b0eaf3931fa90804825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f624cc384c8a07ae7e85833e91f3d1522244a1e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
