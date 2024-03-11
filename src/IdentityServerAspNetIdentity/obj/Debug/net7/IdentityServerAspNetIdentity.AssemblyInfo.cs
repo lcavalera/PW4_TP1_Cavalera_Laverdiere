4ff82925b1d77437c10a8329d50aec86c2f078ef
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-03-11 11:01:54 -0400

    corr identityserver

--- src/IdentityServerAspNetIdentity/obj/Debug/net7/IdentityServerAspNetIdentity.AssemblyInfo.cs
+++ src/IdentityServerAspNetIdentity/obj/Debug/net7/IdentityServerAspNetIdentity.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74193956f7599d703d816ab1f2ee6385e8bf50e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff208f9de672aee037192d11702d7b2daf9fc4c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServerAspNetIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
